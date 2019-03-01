eb0f0f5fb1eedafa2d285a480a6f16b813a117c5
Seth Mcbay <sethmcbay@yahoo.com>
2019-03-01 12:07:33 -0500

    set up comtrol routes

--- seth-mandy/routes/index.js
+++ seth-mandy/routes/index.js
@@ -4,3 +4,24 @@ const router = express.Router()
 
 // Require Controllers
 const slackerController = require('../controllers/slackerController.js')
+
+
+
+router.get('/', slackerController.index)
+router.get('/new', slackerController.new)
+router.post('/', slackerController.create)
+router.get('/:slackId', slackerController.show)
+router.get('/:slackId/edit', slackerController.edit)
+router.put('/:slackId', slackerController.update)
+router.delete('/:slackId', slackerController.delete)
+
+
+
+
+
+
+
+
+
+
+module.exports=router
