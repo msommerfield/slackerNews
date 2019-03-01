6c05c33fdd1dad0b2dff04eee7b2a7ce0c6bf3e9
Mandy <mandysommerfield@gmail.com>
2019-03-01 17:23:42 -0500

    changes to the data in the seeds

--- seth-mandy/db/seed.js
+++ seth-mandy/db/seed.js
@@ -3,36 +3,36 @@ const Slacker = require("../models/Slacker.js");
 
 const sloth = {
     url: "https://en.wikipedia.org/wiki/Sloth",
-    title: "title",
-    description: "description",
+    title: "Sloth",
+    description: "lazy, sometimes green, clawed beasts",
     date: Date.now()
 };
 
 const cat = {
     url: "https://en.wikipedia.org/wiki/Cat",
-    title: "title",
-    description: "description",
+    title: "Cat",
+    description: "Purr, sleepy, yawn",
     date: Date.now()
 };
 
 const puppy = {
-    url: "https://en.wikipedia.org/wiki/puppy",
-    title: "title",
-    description: "description",
+    url: "https://en.wikipedia.org/wiki/Puppy",
+    title: "Dawg",
+    description: "Cuteness overload",
     date: Date.now()
 };
 
 const slug = {
     url: "https://en.wikipedia.org/wiki/Slug",
-    title: "title",
-    description: "description",
+    title: "Slug",
+    description: "Slimy, slow, and gross",
     date: Date.now()
 };
 
 const turtle = {
-    url: "https://en.wikipedia.org/wiki/turtle",
-    title: "title",
-    description: "description",
+    url: "https://en.wikipedia.org/wiki/Turtle",
+    title: "Turtle",
+    description: "Tired Yoshi",
     date: Date.now()
 };
 
