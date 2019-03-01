cc69a374fc88ed6f812484a73a39bae477bc73bf
Mandy <mandysommerfield@gmail.com>
2019-03-01 14:57:30 -0500

    added show connection

--- seth-mandy/controllers/slackerController.js
+++ seth-mandy/controllers/slackerController.js
@@ -2,16 +2,19 @@ const Slacker = require(`../models/Slacker.js`)
 
 const slackerController = {
     index: (req, res) => {
-    //   Slacker.find().then(shops => {
+        //   Slacker.find().then(shops => {
         res.send("testing testing 123")
     },
     new: (req, res) => {
         res.send("testing testing 123")
 
-},
+    },
     create: (req, res) => {
         res.send("i bought a frog")
 
+    },
+    show: (req, res) => {
+        res.send("i bought a foot")
 }
 }
 module.exports = slackerController
