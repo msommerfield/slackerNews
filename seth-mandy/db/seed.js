1874520fef2cc1f2f2a8f2da84e881a6e725ebba
Mandy <mandysommerfield@gmail.com>
2019-03-01 15:48:58 -0500

    added seed data

--- seth-mandy/db/seed.js
+++ seth-mandy/db/seed.js
@@ -1,2 +1,47 @@
 const mongoose = require("./connection.js");
 const Shop = require("../models/Slacker.js");
+
+const sloth = {
+    url: "https://en.wikipedia.org/wiki/Sloth",
+    title: "title",
+    description: "description",
+    date: Date.now()
+};
+
+const cat = {
+    url: "https://en.wikipedia.org/wiki/Cat",
+    title: "title",
+    description: "description",
+    date: Date.now()
+};
+
+const puppy = {
+    url: "https://en.wikipedia.org/wiki/puppy",
+    title: "title",
+    description: "description",
+    date: Date.now()
+};
+
+const slug = {
+    url: "https://en.wikipedia.org/wiki/Slug",
+    title: "title",
+    description: "description",
+    date: Date.now()
+};
+
+const turtle = {
+    url: "https://en.wikipedia.org/wiki/turtle",
+    title: "title",
+    description: "description",
+    date: Date.now()
+};
+
+Slacker.remove({})
+  .then(() => Slacker.create([sloth, cat, puppy, slug, turtle]))
+  .then(() => {
+    console.log("seeded successfully");
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(err, "error!"));
+
+
--- seth-mandy/models/Slacker.js
+++ seth-mandy/models/Slacker.js
@@ -1,2 +1,11 @@
 const mongoose = require('../db/connection.js')
 const Schema = mongoose.Schema
+
+const Slack = new Schema({
+    url: String,
+    title: String,
+    description: String,
+    date: Date
+})
+
+    module.exports = mongoose.model("Slack", Slack)
